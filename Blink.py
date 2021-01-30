d622c1c5345042ee92840a7e1d6edd2bc97f2aaa
Larry Tubbs <larry.tubbs@gmail.com>
2021-01-29 20:48:43 -0600

    added micropy support for pico

--- Blink.py
+++ Blink.py
@@ -4,17 +4,16 @@ led_onboard = machine.Pin(15, machine.Pin.OUT)
 
 def dah():
     light(.25)
-    
+
 def dash():
     light(.5)
-    
+
 def light(delay):
     led_onboard.value(1)
     utime.sleep(delay)
     led_onboard.value(0)
     utime.sleep(delay)
-    
-    
+
 while True:
     dah()
     dah()
